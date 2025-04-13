a74e396a3c58a5b9542a17f89c54e0d27629f296
kristiyan.yanchev <kristiyan.yancheff@gmail.com>
2025-04-13 22:16:47 +0300

    adde docker compose

---
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
