d20b54d055d66a787b3b8395bf9af7b6c3360e4d
Ivanka Todorova <iv@itodorova.dev>
2025-04-19 12:02:54 +0300

    chore: rename `DemoApplication` to something more meaningful

--- backend/src/main/java/edu/nbu/team13/gradecenter/GradeCenterApplication.java
+++ backend/src/main/java/edu/nbu/team13/gradecenter/GradeCenterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class GradeCenterApplication {
 
     @GetMapping("/hello")
     public String hello() {
@@ -15,7 +15,7 @@ public class DemoApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(GradeCenterApplication.class, args);
     }
 
 }
