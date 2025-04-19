45b5b1aa2d9d02976627bf4ae4c79a9c60e48158
Ivanka Todorova <iv@itodorova.dev>
2025-04-19 13:26:59 +0300

    chore: setup jdbc to work with postgresql (run tests in memory)

---
+++ backend/src/main/java/edu/nbu/team13/gradecenter/entities/User.java
@@ -0,0 +1,4 @@
+package edu.nbu.team13.gradecenter.entities;
+
+public class User {
+}
--- backend/src/test/java/edu/nbu/team13/gradecenter/GradeCenterApplicationTests.java
+++ backend/src/test/java/edu/nbu/team13/gradecenter/GradeCenterApplicationTests.java
@@ -2,7 +2,9 @@ package edu.nbu.team13.gradecenter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class GradeCenterApplicationTests {
 
